f342ea95f906cfc128c6526541db7c3764328f86
k3nt0w <kento.watanabe.11@gmail.com>
2020-01-25 22:47:23 +0900

    chore: tsconfig

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin') //installed via npm
+const webpack = require('webpack') //to access built-in plugins
+
+module.exports = {
+  mode: 'production',
+  entry: ['./src/index.ts'],
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+
+  devtool: 'inline-source-map',
+  target: 'web',
+
+  node: {
+    __dirname: false,
+    __filename: false
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunksSortMode: 'dependency',
+      template: path.resolve(__dirname, 'src/index.html')
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loaders: ['ts-loader']
+      }
+    ]
+  }
+}
